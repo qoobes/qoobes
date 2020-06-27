4a2342ddd2f03fac320f3073015998b73b38c7d4
theqoobee <qoobeethegreat@gmail.com>
2020-06-27 19:01:33 +0200

    No small displays here

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 'use strict'
 
-document.addEventListener("DOMContentLoaded", function(event) { 
+// document.addEventListener("DOMContentLoaded", function(event) { 
     // window.scrollTo(0, 0)
-  });
+    setInterval(() => {
+       if (window.innerWidth < 760) {
+         document.body.innerHTML = ''
+         document.write('this website does not work on smaller screens')
+    }, 1000)
+  // });
