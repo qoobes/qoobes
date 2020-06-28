53e4d4d87c97707ba13dd42570f905df8744f611
theqoobee <qoobeethegreat@gmail.com>
2020-06-28 14:05:40 +0200

    Animations work better

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 'use strict'
 
-// document.addEventListener("DOMContentLoaded", function(event) { 
-    // window.scrollTo(0, 0)
-    setInterval(() => {
-       if (window.innerWidth < 760) {
-         document.body.innerHTML = ''
-         document.write('this website does not work on smaller screens')
-    }, 1000)
-  // });
+document.addEventListener("DOMContentLoaded", function(event) { 
+  // window.scrollTo(0, 0)
+  document.querySelector('.anim').classList.remove('hidden')
+  // animate()
+  setTimeout(() => {
+    stopAnimation()
+  }, 8500)
+});
+
+const stopAnimation = () => {
+  document.querySelector('#animationContainer').remove()
+  document.querySelector('body').classList.remove('overflow-hidden')
+}
+
+setInterval(() => {
+    if (window.innerWidth < 760) {
+      document.body.innerHTML = ''
+      document.write('this website does not work on smaller screens')
+}}, 1000)
+
+  // Make the animation work
